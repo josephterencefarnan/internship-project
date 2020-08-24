5c0e2b0208cd16fc64bbdcea0d395867fb391d29
josephterencefarnan <69175515+josephterencefarnan@users.noreply.github.com>
2020-08-24 17:07:05 -0400

    commit

--- src/lib/jit/index.js
+++ src/lib/jit/index.js
@@ -1,9 +1,42 @@
 var JIT = {};
 (function () {
-    function userInterface() {
-        return "Hello World";
+    function calculateDelta(oldDelta, oldNumber, newNumber, indexOfOldNum){
+        return((oldDelta * indexOfOldNum) + (newNumber - oldNumber))/(indexOfOldNum + 1)
+    }
+    function userInterface(input, signifigance, threshhold) {
+        const sorted = input.slice().sort((a, b) => { return a[0] - b[0]; })
+        var delta = 0;
+        var dateDelta = input[0][0]
+        var valuableValues
+        if (signifigance === undefined) {
+            valuableValues = sorted
+        }
+        else {
+            valuableValues = sorted.filter(function(x){
+                return x[1] === signifigance
+            })
+        }
+
+        valuableValues.forEach((x, i, a) => {
+            if (i < valuableValues.length - 1){
+                delta = calculateDelta(delta, x[2], valuableValues[i+1][2], i)                
+                dateDelta = calculateDelta(delta, x[0].getTime(), valuableValues[i+1][0].getTime(), i)
+            } 
+        });
+
+        const output = sorted.map((x, i, a) => {
+            return [x[0], x[2]]
+        })
+        if (threshhold !== undefined){
+            while (output[output.length - 1][1] < threshhold){
+                newDate = new Date(output[output.length -1][0].getTime() + dateDelta)
+            output.push([newDate, output[output.length - 1][1] + delta])
+            }
+        }
+        return output;
     }
 
 
     JIT.userInterface = userInterface;
+    JIT.calculateDelta = calculateDelta;
 })();
--- test/jit/index.js
+++ test/jit/index.js
@@ -1,8 +1,44 @@
 var expect = chai.expect;
 describe("JIT", function () {
     describe("userInterface", function () {
-        it("return Hello World", function () {
-            expect(JIT.userInterface()).to.equal("Hello World")
+        it.skip("return all readings", function () {
+            const result = JIT.userInterface(sampleInput, "1", 13)
+            expect(result).to.eql([10, 11, 11, 12, 13])
+ //           expect(JIT.userInterface(sampleInput, "0")).to.eql([10, 11, 11, 12, 12])
+            expect(JIT.userInterface(sampleInput, undefined, 12.5)).to.eql([10, 11, 11, 12, (12 + 2/3)])
+            
+        })
+        it.skip("projectsToAThreshhold", function(){
+            const result = JIT.userInterface(sampleInput, "1", 15)
+            expect(result).to.eql([10, 11, 11, 12, 13, 14, 15])
+  
+        })
+        it("addsDates", function(){
+            const result = JIT.userInterface(sampleInput, "1", 13)
+            expect(result).to.eql([
+                [new Date("2020-08-10T15:32:45.123Z"), 10],
+                [new Date("2020-08-11T15:32:45.123Z"), 11],
+                [new Date("2020-08-12T15:32:45.123Z"), 11],
+                [new Date("2020-08-13T15:32:45.123Z"), 12],
+                [new Date("2020-08-14T15:32:45.123Z"), 13]
+            ])
+        })
+    })
+    describe("calculateDelta", function (){
+        it("derivesValue", function () {
+            let delta = JIT.calculateDelta(1, 12, 13, 1)
+            expect(delta).to.eql(1)
+            delta = JIT.calculateDelta(2, 12, 14, 1)
+            expect(delta).to.eql(2)
+            delta = JIT.calculateDelta(5, 12, 14, 2)
+            expect(delta).to.eql(4)            
         })
     })
 })
+var sampleOutput = [5, 10, 9, 8, 75, 350, 400]
+var sampleInput = [
+    [new Date("2020-08-10T15:32:45.123Z"), "1", 10],
+    [new Date("2020-08-13T15:32:45.123Z"), "1", 12],
+    [new Date("2020-08-11T15:32:45.123Z"), "1", 11],
+    [new Date("2020-08-12T15:32:45.123Z"), "0", 11]
+]
